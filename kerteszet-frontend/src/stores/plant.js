207b16a2a59661a3ca06ecd6d606d7649444709d
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-03-06 20:29:52 +0100

    meglévő növény megjelenik

---
+++ kerteszet-frontend/src/stores/plant.js
@@ -0,0 +1,19 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+export const usePlantStore = defineStore('plant', () => {
+  const plants = ref({})
+  
+  const fetchPlants = async() =>{
+    try{
+      const response = await axios.get("http://localhost:3000/plants")
+      plants.value = response.data
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
+
+  return { plants, fetchPlants }
+})
--- kerteszet-frontend/src/views/HomeView.vue
+++ kerteszet-frontend/src/views/HomeView.vue
@@ -1,9 +1,36 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { usePlantStore } from '@/stores/plant.js'
+import { onMounted } from 'vue'
+
+const plantStore = usePlantStore()
+
+onMounted(async () => {
+  await plantStore.fetchPlants()
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="container mt-4 mb-4">
+    <div v-if="plantStore.plants.length > 0" class="row justify-content-center gap-3">
+      <div v-for="plant in plantStore.plants" :key="plant.id" class="col-12 col-md-4 col-lg-4">
+        <div class="card bg-dark text-white shadow-lg">
+          <div class="card-body">
+            <h5 class="card-title">{{ plant.name }}</h5>
+            <p class="card-text">
+              <strong>Évelő?</strong> {{ plant.perennial ? 'Igen' : 'Nem' }}<br />
+              <strong>Kategória:</strong> {{ plant.category }} <br />
+              <strong>Ár:</strong> {{ plant.price }}
+            </p>
+            <button class="btn btn-primary w-100" @click="">
+              Szerkesztés
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div v-else class="text-center text-white mt-4">
+      <p>Nincsenek növények.</p>
+    </div>
   </main>
 </template>
