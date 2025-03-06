6075b712b6bac650c7ff4ea1131f9bd956900a8a
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-03-06 18:56:18 +0100

    adatbázis feltöltése és javítása

--- backend/database.js
+++ backend/database.js
@@ -5,13 +5,25 @@ const db = new sqlite3.Database("./database.sqlite3");
 const initDb = async() =>{
     await dbRun("DROP TABLE IF EXISTS plants");
     await dbRun(`CREATE TABLE IF NOT EXISTS plants(
-        id int PRIMARY KEY AUTOINCREMENT,
-        name TEXT
-        perennial BOOLEAN
-        category TEXT
-        price REAL
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        perennial INTEGER,
+        category TEXT,
+        price REAL,
         )`);
-        
+
+    const plants = [
+        {
+            name: "rózsa",
+            perennial: 1,
+            category: "virág",
+            price: 12.99,
+        }
+    ];
+
+    for(const plant in plants){
+        await dbRun("INSERT INTO plants (name,perennial,category,price) VALUES(?,?,?,?)",[plant.name,plant.perennial,plant.category,plant.price]);
+    }
 }
 
 export {db, initDb}
